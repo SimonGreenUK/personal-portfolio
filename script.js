6631df117dafbd4839538dcca052ec595206ead6
Simon Green <simonisaacgreen@icloud.com>
2019-05-07 23:02:22 +0100

    Mobile menu closes when mobile menu link is clicked

--- script.js
+++ script.js
@@ -1,20 +1,13 @@
 const mobile_menu_toggle = document.getElementsByClassName('mobile-menu-toggle')[0];
-const mobile_menu_links = document.getElementsByClassName('mobile-menu-link');
+const mobile_menu_links = document.querySelectorAll('.mobile-menu-link');
 const mobile_menu = document.getElementsByClassName('mobile-menu')[0];
 
-// CLICK HAMBURGER TO SHOW MOBILE MENU
-function toggleMobileMenu(click) {
+// TOGGLE SHOW MOBILE MENU ON HAMBURGER OR MOBILE MENU LINK CLICK
+function toggleOpenMobileMenu(click) {
     mobile_menu.classList.toggle('mobile-menu-visible');
 }
 
-mobile_menu_toggle.addEventListener('click', toggleMobileMenu);
-
-// MOBILE MENU CLOSES WHEN MOBILE MENU LINK CLICKED
-function mobileMenuLinks() {
-    console.log(mobile_menu_links);
-}
-
-mobileMenuLinks();
+mobile_menu_toggle.addEventListener('click', toggleOpenMobileMenu);
 
 
-
+mobile_menu_links.forEach(link => link.addEventListener('click', toggleOpenMobileMenu));
