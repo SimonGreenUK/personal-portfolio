00226efadebafe2e28427c1dbb8b30738fad54ee
Simon Green <simonisaacgreen@icloud.com>
2019-05-08 20:22:19 +0100

    Updating styles to BEM initial phase

--- script.js
+++ script.js
@@ -2,12 +2,11 @@ const mobile_menu_toggle = document.getElementsByClassName('mobile-menu-toggle')
 const mobile_menu_links = document.querySelectorAll('.mobile-menu-link');
 const mobile_menu = document.getElementsByClassName('mobile-menu')[0];
 
-// TOGGLE SHOW MOBILE MENU ON HAMBURGER OR MOBILE MENU LINK CLICK
+// SHOW MOBILE MENU ON HAMBURGER OR MOBILE MENU LINK CLICK
 function toggleOpenMobileMenu(click) {
     mobile_menu.classList.toggle('mobile-menu-visible');
 }
 
 mobile_menu_toggle.addEventListener('click', toggleOpenMobileMenu);
-
 
 mobile_menu_links.forEach(link => link.addEventListener('click', toggleOpenMobileMenu));
