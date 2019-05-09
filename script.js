b9753645df2ec751f000ecbd0fc13c3c6a656196
Simon Green <simonisaacgreen@icloud.com>
2019-05-09 21:25:51 +0100

    Adding animated mobile menu

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const mobile_menu_toggle = document.getElementsByClassName('mobile-menu-toggle')[0];
+const mobile_menu_toggle_bars = document.querySelectorAll('.mobile-menu-toggle-bars');
 const mobile_menu_links = document.querySelectorAll('.mobile-menu-link');
 const mobile_menu = document.getElementsByClassName('mobile-menu')[0];
+
+// ANIMATE HAMBURGER ON CLICK 
+function animateHamburger(click) {
+    mobile_menu_toggle.classList.toggle('change');
+}
+
+mobile_menu_toggle.addEventListener('click', animateHamburger);
+
 
 // SHOW MOBILE MENU ON HAMBURGER OR MOBILE MENU LINK CLICK
 function toggleOpenMobileMenu(click) {
