86dd9b6a4caddc45ebdbc71bbab95bd696620128
Simon Green <simonisaacgreen@icloud.com>
2019-05-12 20:18:39 +0100

    Hiding nav-title when on the welcome-section

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const mobile_menu_icon = document.getElementsByClassName('mobile-menu-icon')[0];
 const mobile_menu_icon_bars = document.querySelectorAll('.mobile-menu-icon-bars');
 const mobile_menu_links = document.querySelectorAll('.mobile-menu-link');
 const mobile_menu = document.getElementsByClassName('mobile-menu')[0];
+const nav_title = document.getElementsByClassName('nav-title')[0];
 
 // ANIMATE HAMBURGER ON CLICK 
 function animateHamburger(click) {
@@ -20,3 +21,22 @@ mobile_menu_icon.addEventListener('click', animateHamburger);
 mobile_menu_links.forEach(link => link.addEventListener('click', toggleOpenMobileMenu));
 
 mobile_menu_links.forEach(link => link.addEventListener('click', animateHamburger));
+
+// SHOW MENU TITLE WHEN NOT ON WELCOME SECTION
+let scrollPos = 0;
+
+function checkPosition() {
+    
+  let windowY = window.scrollY;
+  console.log(windowY);
+  if (windowY > 505) {
+    nav_title.classList.remove('nav-title--hidden');
+    nav_title.classList.add('nav-title--visible');
+  } else {
+    nav_title.classList.add('nav-title--hidden');
+    nav_title.classList.remove('nav-title--visible');
+  }
+  scrollPos = windowY;
+}
+
+window.addEventListener('scroll', checkPosition);
