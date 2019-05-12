e71f1fd664cb225e04a7a7658a832979bc719631
Simon Green <simonisaacgreen@icloud.com>
2019-05-12 19:26:56 +0100

    Updating hamburger icon style and style names

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-const mobile_menu_toggle = document.getElementsByClassName('mobile-menu-toggle')[0];
-const mobile_menu_toggle_bars = document.querySelectorAll('.mobile-menu-toggle-bars');
+const mobile_menu_icon = document.getElementsByClassName('mobile-menu-icon')[0];
+const mobile_menu_icon_bars = document.querySelectorAll('.mobile-menu-icon-bars');
 const mobile_menu_links = document.querySelectorAll('.mobile-menu-link');
 const mobile_menu = document.getElementsByClassName('mobile-menu')[0];
 
 // ANIMATE HAMBURGER ON CLICK 
 function animateHamburger(click) {
-    mobile_menu_toggle.classList.toggle('change');
+    mobile_menu_icon.classList.toggle('change');
 }
 
-mobile_menu_toggle.addEventListener('click', animateHamburger);
+mobile_menu_icon.addEventListener('click', animateHamburger);
 
 
 // SHOW MOBILE MENU ON HAMBURGER OR MOBILE MENU LINK CLICK
@@ -16,6 +16,6 @@ function toggleOpenMobileMenu(click) {
     mobile_menu.classList.toggle('mobile-menu-visible');
 }
 
-mobile_menu_toggle.addEventListener('click', toggleOpenMobileMenu);
+mobile_menu_icon.addEventListener('click', toggleOpenMobileMenu);
 
 mobile_menu_links.forEach(link => link.addEventListener('click', toggleOpenMobileMenu));
