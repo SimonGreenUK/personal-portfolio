ce9cf3cfd415691fc89531d46cf19ef1d03ddae2
Simon Green <simonisaacgreen@icloud.com>
2019-05-30 22:41:16 +0100

    Nav title hidden when welcome social icons visible

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const mobile_menu_icon = document.getElementsByClassName('mobile-menu-icon')[0];
 const mobile_menu_icon_bars = document.querySelectorAll('.mobile-menu-icon-bars');
 const mobile_menu_links = document.querySelectorAll('.mobile-menu-link');
 const mobile_menu = document.getElementsByClassName('mobile-menu')[0];
+const welcome_section_content_icons = document.getElementsByClassName('welcome-section-content-icons')[0];
 const nav_title = document.getElementsByClassName('nav-title')[0];
 
 // ANIMATE HAMBURGER ON CLICK 
@@ -22,15 +23,24 @@ mobile_menu_links.forEach(link => link.addEventListener('click', toggleOpenMobil
 
 mobile_menu_links.forEach(link => link.addEventListener('click', animateHamburger));
 
-// SHOW NAV TITLE WHEN NOT ON WELCOME SECTION
-function checkPosition() {
-  if (window.scrollY > 525) {
-    nav_title.classList.remove('nav-title--hidden');
-    nav_title.classList.add('nav-title--visible');
-  } else {
+// HELPER FUNCTION - CHECKS IF PASSED IN ELEMENT IS IN VIEWPORT & RETURNS BOOLEAN
+let isInViewport = function (elem) {
+  let bounding = elem.getBoundingClientRect();
+  return (
+      bounding.top >= 0 &&
+      bounding.left >= 0 &&
+      bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+};
+
+// HIDE NAV TITLE WHEN WELCOME SECTION ICONS IN VIEWPORT
+window.addEventListener('scroll', function (event) {
+  if (isInViewport(welcome_section_content_icons)) {
     nav_title.classList.add('nav-title--hidden');
     nav_title.classList.remove('nav-title--visible');
-  }
-}
-
-window.addEventListener('scroll', checkPosition);
+  } else {
+      nav_title.classList.remove('nav-title--hidden');
+      nav_title.classList.add('nav-title--visible');  
+    }
+});
